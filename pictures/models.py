7fe46fab6d90e0570a81b055ed1936bcf7e36a82
FlakoBB <josephrbb99@gmail.com>
2023-12-01 13:49:09 -0600

    update: modify the save method of Picture model

--- pictures/models.py
+++ pictures/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import os
+from django.utils.text import slugify
 
 class Album(models.Model):
   name = models.CharField(max_length=150, default='Nameless Album', help_text='Nombre del album')
@@ -14,6 +15,17 @@ class Picture(models.Model):
   title = models.CharField(max_length=150, default='Nameless Picture', help_text='Titulo de la imagen')
   description = models.CharField(max_length=300, help_text='descripcion de la imagen', blank=True, null=True)
   album = models.ForeignKey(Album, help_text='A que album pertenece', blank=True, null=True, on_delete=models.SET_NULL)
+
+  def save(self, *args, **kwargs):
+    original_name = os.path.basename(self.image.name)
+
+    new_name = slugify(self.title)
+
+    final_name = f'{new_name}{os.path.splitext(original_name)[1]}'
+
+    self.image.name = final_name
+
+    super().save(*args, **kwargs)
 
   def delete(self, using=False, keep_parents=False):
     if self.image:
